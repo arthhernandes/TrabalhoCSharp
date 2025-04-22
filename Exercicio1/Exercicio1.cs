3c147a8302b0b7e72efc4b6648119298b8af1d95
Arthur Hernandes <arthur07hernandes@hotmail.com>
2025-04-22 14:22:31 -0300

    Create Exercicio1.cs

---
+++ Exercicio1/Exercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Exercicio1{
+
+    static void Main(){
+
+        Console.WriteLine("Crie sua senha: ");
+        string senha = Console.ReadLine();
+
+        if (senha.Length >= 8 && senha.IndexOfAny("ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()) != -1 &&
+            senha.IndexOfAny("0123456789".ToCharArray()) != -1 && 
+            senha.IndexOfAny("!@#$%^&*(),.?\":{}|<>".ToCharArray()) != -1){
+            Console.WriteLine("Senha válida!");
+        }else{
+            Console.WriteLine("Senha inválida. Certifique-se de que ela atende aos critérios.");
+        }
+    }
+}
