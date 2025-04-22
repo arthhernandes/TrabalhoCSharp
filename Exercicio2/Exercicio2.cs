eda9258fa23c22eb9fe9dc65b86cbc19af03023e
Arthur Hernandes <arthur07hernandes@hotmail.com>
2025-04-22 14:33:03 -0300

    Create Exercicio2.cs

---
+++ Exercicio2/Exercicio2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Exercicio2{
+
+    static void Main(){
+
+        Console.WriteLine("Digite a tabuada que deseja saber: ");
+        int tabuada = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i <= 10; i++){
+            Console.WriteLine($"{tabuada} x {i} = {tabuada * i}");
+        }
+    }
+}
