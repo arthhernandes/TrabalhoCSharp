56190389c30317690cc763344b044d5a0ff9f22f
Arthur Hernandes <arthur07hernandes@hotmail.com>
2025-04-22 14:41:37 -0300

    Create Exercicio3.cs

---
+++ Exercicio3/Exercicio3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+class Exercicio3{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número para calcular o fatorial:");
+        int numero = int.Parse(Console.ReadLine());
+        int fatorial = 1;
+
+        while (numero > 1){
+            fatorial *= numero;
+            numero--;
+        }
+
+        Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+    }
+}
